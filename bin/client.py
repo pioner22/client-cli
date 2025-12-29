bb1920922930af366f28ea2b46a937a7e3073ee4
pioner22 <dimanger@gmail.com>
2025-12-30 01:43:43 +0300

    Bump client version to 0.4.1599

--- bin/client.py
+++ bin/client.py
@@ -1149,7 +1149,7 @@ except Exception:
     def get_file_system_suggestions(token: str, cwd=None, limit: int = 20):
         return []
 
-CLIENT_VERSION = "0.4.1597"
+CLIENT_VERSION = "0.4.1599"
 _VER_PART_RE = re.compile(r"\d+")
 
 

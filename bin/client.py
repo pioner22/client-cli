fdb437f8ceb91661502395c2b88db19445196adb
pioner22 <dimanger@gmail.com>
2026-06-05 11:33:22 +0300

    Bump CLI client to 0.4.2185

--- bin/client.py
+++ bin/client.py
@@ -837,7 +837,7 @@ except Exception:
     def get_file_system_suggestions(token: str, cwd=None, limit: int = 20):
         return []
 
-CLIENT_VERSION = "0.4.2175"
+CLIENT_VERSION = "0.4.2185"
 _VER_PART_RE = re.compile(r"\d+")
 
 
